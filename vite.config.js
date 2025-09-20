1ecd846f6317ac58f8e6d0d1c2ef06a45d8190b3
OLDmamorim <mramorim78@gmail.com>
2025-09-20 18:32:59 +0100

    Atualizar o vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: false
+  },
   build: {
-    outDir: 'dist',
     minify: false
   }
 })
